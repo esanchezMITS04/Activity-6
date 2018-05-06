9087deffbb3dad0061f1b2cf9332aa180a12ab17
admin <admin@admins-iMac-24.local>
2018-05-06 15:44:17 +0800

    activity number 6 commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :client_workouts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'client_workouts/find' => 'client_workouts#find'
+end
